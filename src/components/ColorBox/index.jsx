4bf5af1fda9aa156e34e9237f1da749a4ff2fa5d
THANHPM <minhthanh95ptit@gmail.com>
2020-06-04 08:37:43 +0700

    useState()

---
+++ src/components/ColorBox/index.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import './ColorBox.scss';
+
+ColorBox.propTypes = {
+
+};
+
+function getRandomColor() {
+    const COLOR_LIST = ['deeppink', 'green', 'yellow', 'black', 'blue'];
+    const randomIndex = Math.trunc(Math.random() * 4); // Math.trunc - phan nguyen
+    return COLOR_LIST[randomIndex];
+}
+
+
+function ColorBox() {
+
+    const [color, setColor] = useState(() => {
+        const initColor = localStorage.getItem('box_color') || 'deeppink';
+        console.log(initColor);
+        return initColor;
+    });
+
+    function handleBoxClick() {
+        const newColor = getRandomColor();
+        setColor(newColor);
+
+        localStorage.setItem('box_color', newColor);
+    }
+
+    return (
+        <div
+            className="color-box"
+            style={{ backgroundColor: color }}
+            onClick={handleBoxClick}
+        >
+        </div>
+    );
+}
+
+export default ColorBox;
